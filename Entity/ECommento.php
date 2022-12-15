c145ca49f761fe512eaaee1fcd25065a974001c7
isvalsc <vscimia00@gmail.con>
2022-12-15 21:04:57 +0100

    view singolo prodotto

--- Entity/ECommento.php
+++ Entity/ECommento.php
@@ -3,17 +3,18 @@
  * TO BE CHECKED
  * @package Entity
  */
-class ECommento {
-	/**
-	 * @AttributeType int
-	 */
-	public $id;
-	/**
-	 * @AttributeType string
-	 */
-	public $testo;
+class ECommento
+{
+    /**
+     * @AttributeType int
+     */
+    public $id;
+    /**
+     * @AttributeType string
+     */
+    public $testo;
 
-	public $_SERVER['127.0.0.1']
+}
 
 	//get
 

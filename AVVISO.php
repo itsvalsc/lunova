eb8a632b43bdff069c7ed6da6eb1ae2a5d5796c9
itvalsc <vscimia00@gmail.com>
2023-02-20 22:25:22 +0100

    AVVISO.php

--- AVVISO.php
+++ AVVISO.php
@@ -1,2 +1,20 @@
-<?php
-//TODO: AVVISARE PRIMA DI FARE LE COSE SOPRATTUTTO SE NON PATTUITE :)
+
+//TODO: cose da fare
+
+
+
+
+rendere tutto dinamico sessioni
+
+admin attaccare bottoni...
+metodo di blocco utente
+commenti + funzione censura da mettere nel controllore
+
+
+modificare da eUtente e EDiso a semplicemente gli id
+ricerca per artista nei prod generici, c'è una funzione in artista da poter tenere i considerazione che prende id artista e restituisce il nome
+
+
+
+
+

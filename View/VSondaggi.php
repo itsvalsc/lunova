2679502baba4b19cc85ee4341391ea800f93dfe3
luibart <bartolomeoluigi85@gmail.com>
2023-04-13 14:07:43 +0200

    sond error fix

--- View/VSondaggi.php
+++ View/VSondaggi.php
@@ -28,9 +28,15 @@ class VSondaggi{
     public function show(ESondaggio $s,$votazione,$logged, $num)
     {
         $totale = $s->getVotiDisco1()+$s->getVotiDisco2()+$s->getVotiDisco3();
-        $perc1 = ($s->getVotiDisco1()*100)/$totale;
-        $perc2 = ($s->getVotiDisco2()*100)/$totale;
-        $perc3 = ($s->getVotiDisco3()*100)/$totale;
+        if ($totale==0){
+            $perc1=0;
+            $perc2=0;
+            $perc3=0;
+        }else{
+            $perc1 = ($s->getVotiDisco1()*100)/$totale;
+            $perc2 = ($s->getVotiDisco2()*100)/$totale;
+            $perc3 = ($s->getVotiDisco3()*100)/$totale;
+        }
         $array = [$perc1,$perc2,$perc3,$totale];
 
 
